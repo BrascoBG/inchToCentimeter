5027764501927cfa744080b008dc54b022bdffd7
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-01-09 16:11:37 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+function InchToCentimeter(a){
+  let inch = parseFloat(a);
+  let centimeters = inch * 2.54;
+  console.log(inch + " inches are " + centimeters + " centimeters");
+}
+InchToCentimeter(5);
